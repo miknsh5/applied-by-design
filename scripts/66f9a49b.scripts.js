5c0d5b1d9ca7cef86c65659f6fcd9d3d6015310f
miknsh5 <mnash5@gmail.com>
2013-06-06 13:43:34 -0700

    -n Initial dist commit

---
+++ scripts/66f9a49b.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("appliedByDesignApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("appliedByDesignApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
