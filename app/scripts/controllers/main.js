a6256b41cf4663262ac83ab872585159a9db2754
miknsh5 <mnash5@gmail.com>
2013-06-26 21:26:19 -0700

    new dist

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,40 +2,27 @@
 
 angular.module('appliedByDesignApp')
     .controller('MainCtrl', function ($scope) {
-        //main page controller
-        //initialize visibility flag
-        // $scope.style_title_dev = {'margin-top': '-59px', 'opacity':'1', 'transition': 'opacity 1s'};
-        $scope.style_title_str = {};
-        $scope.style_title_des = {};
+      $scope.awesomeThings = ['hello', 'he', 'dsfg'];
 
-        $scope.triggerUpdate2 = function() {
-            // $('.section-title').toggleClass(".section-title-hide");
-            document.querySelector('.section-title').style.transition="margin-top 1s";
-            document.querySelector('.section-title').style.marginTop=-110+"px";
-            document.querySelector('.section-title').style.opacity="0";
-            // document.querySelector('.section-title').style.transition="opacity 1s";
-            // document.querySelector('.section-title').toggleClass(".section-title-hide");
-        }
+    });
+    // .controller('MainCtrl', function ($scope) {
+        // $scope.test = 'hey';
+        // $scope.triggerUpdate2 = function() {
+        //     // $('.section-title').toggleClass(".section-title-hide");
+        //     document.querySelector('.section-title').style.transition='margin-top 1s';
+        //     document.querySelector('.section-title').style.marginTop=-110+'px';
+        //     document.querySelector('.section-title').style.opacity='0';
+        // }
 
-        $scope.triggerUpdate = function() {
+        // $scope.triggerUpdate = function() {
 
-            //trigger the DOM boolean here
-            $scope.style_fade_dev = {'margin-top': '-110px', 'opacity':'0'};
-            $scope.style_slide_dev = {'margin-top': '-110px', 'transition': 'margin-top 1s'};
-            $scope.$apply();
+        //     //trigger the DOM boolean here
+        //     $scope.style_fade_dev = {'margin-top': '-110px', 'opacity':'0'};
+        //     $scope.style_slide_dev = {'margin-top': '-110px', 'transition': 'margin-top 1s'};
+        //     $scope.$apply();
 
-        }
-    })
-    .controller('MainSectionCtrl', function ($scope){
-        // subpage controller for each section on the main page
-
-        // setup a watch on the window scroll position
-        // need to figure out when the icon blocks make it
-        // to the center of the screen and then make the 
-        // css update to trigger the css3 animations
-        
-
-    })    
+        // }
+      // });
 
     // // parallax
     //   function blockIconTrigger() {
