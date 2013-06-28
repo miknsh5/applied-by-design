a155cc3634dd4eee9d49a4333ffca123b482e705
miknsh5 <mnash5@gmail.com>
2013-06-28 00:45:02 -0700

    parallaxified

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,15 +18,69 @@ angular.module('appliedByDesignApp')
       $scope.toggleState = function(){
         $scope.isStVisible = !$scope.isStVisible;
         if ($scope.isStVisible) {
-          $('.block-lg').css({'width': '240px'});
-          $('.long-title').css({'opacity': 1});
+          $('.block-dynamic').css({'width': '240px'});
           $('.short-title').css({'opacity': 0});
+          $('.long-title').css({'opacity': 1});
         } else {
-          $('.block-lg').css({'width': '80px'});
-          $('.long-title').css({'opacity': 0});
+          $('.block-dynamic').css({'width': '80px'});
           $('.short-title').css({'opacity': 1});
+          $('.long-title').css({'opacity': 0});
         }
 
       };
+      $scope.toggleState();
 
-    });
+
+
+
+    var $strategy = $('section.strategy');
+    var $design = $('section.design');
+    var $development = $('section.development');
+    var $summary = $('section.summary');
+    var windowScroll;
+    var isMobile = false;
+
+    // Identify if visitor has a large enough viewport for parallaxing title
+    function isLargeViewport() {
+        return true;
+    }
+
+    // If large viewport and not mobile, parallax the title
+    if(!isMobile) {
+      $(window).scroll(function() {
+        if(isLargeViewport()) {
+          parallaxify();
+        }
+      });
+    }
+
+    // margin-top shims to compensate for parallax as you progress further down the page 
+    var strategyShim = -20;
+    var designShim = 75;
+    var developmentShim = 150;
+    var summaryShim = 225;
+
+    function parallaxify() {
+      //Get scroll position of window
+      windowScroll = $(window).scrollTop();
+      var plaxFactor = 4;
+      //parallax the content section in front of the images
+      $strategy.css({
+        'margin-top' : (-windowScroll/plaxFactor + strategyShim)+"px"
+      });
+      $design.css({
+        'margin-top' : (-windowScroll/plaxFactor + designShim)+"px"
+      });
+      $development.css({
+        'margin-top' : (-windowScroll/plaxFactor + developmentShim)+"px"
+      });
+      $summary.css({
+        'margin-top' : (-windowScroll/plaxFactor + summaryShim)+"px"
+      });
+    }
+
+  });
+
+
+
+
