8bff4f4d6c2ba704fba43f00483ae51e33d257b8
miknsh5 <mnash5@gmail.com>
2013-06-28 09:39:49 -0700

    disabling parallax to test IE8

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -31,87 +31,87 @@ angular.module('appliedByDesignApp')
       // $scope.toggleState();
 
 
-      var isMobile;
+      // var isMobile;
 
-      //figure out which platform the user's on
-      if( navigator.userAgent.match(/Android/i) ||
-          navigator.userAgent.match(/webOS/i) ||
-          navigator.userAgent.match(/iPhone/i) ||
-          navigator.userAgent.match(/iPod/i) ||
-          navigator.userAgent.match(/iPad/i) ||
-          navigator.userAgent.match(/BlackBerry/)
-      ){
-        isMobile = true;
-      }
+      // //figure out which platform the user's on
+      // if( navigator.userAgent.match(/Android/i) ||
+      //     navigator.userAgent.match(/webOS/i) ||
+      //     navigator.userAgent.match(/iPhone/i) ||
+      //     navigator.userAgent.match(/iPod/i) ||
+      //     navigator.userAgent.match(/iPad/i) ||
+      //     navigator.userAgent.match(/BlackBerry/)
+      // ){
+      //   isMobile = true;
+      // }
 
-      var $strategy = $('section.strategy');
-      var $design = $('section.design');
-      var $development = $('section.development');
-      var $summary = $('section.summary');
-      var windowScroll;
+      // var $strategy = $('section.strategy');
+      // var $design = $('section.design');
+      // var $development = $('section.development');
+      // var $summary = $('section.summary');
+      // var windowScroll;
 
-      // Check window width - if less than 1000px disable parallax
-      function isLargeViewport() {
-        if($(window).width() < 979) {
-          return false;
-        } else {
-          return true;
-        }
-      }
+      // // Check window width - if less than 1000px disable parallax
+      // function isLargeViewport() {
+      //   if($(window).width() < 979) {
+      //     return false;
+      //   } else {
+      //     return true;
+      //   }
+      // }
 
-      // initialize the margin-tops to '0'.  Only adjust if browser passes
-      $strategy.css({
-        'margin-top' : '0px'
-      });
-      $design.css({
-        'margin-top' : '0px'
-      });
-      $development.css({
-        'margin-top' : '0px'
-      });
-      $summary.css({
-        'margin-top' : '0px'
-      });
+      // // initialize the margin-tops to '0'.  Only adjust if browser passes
+      // $strategy.css({
+      //   'margin-top' : '0px'
+      // });
+      // $design.css({
+      //   'margin-top' : '0px'
+      // });
+      // $development.css({
+      //   'margin-top' : '0px'
+      // });
+      // $summary.css({
+      //   'margin-top' : '0px'
+      // });
 
-      // recalc parallax offsets if window size changes
-      $(window).resize(function() {
-          parallaxify();
-        });
+      // // recalc parallax offsets if window size changes
+      // $(window).resize(function() {
+      //     parallaxify();
+      //   });
 
-      // If large viewport and not mobile, parallax the sections
-      if(!isMobile) {
-        $(window).scroll(function() {
-          if(isLargeViewport()) {
-            parallaxify();
-          }
-        });
-      }
+      // // If large viewport and not mobile, parallax the sections
+      // if(!isMobile) {
+      //   $(window).scroll(function() {
+      //     if(isLargeViewport()) {
+      //       parallaxify();
+      //     }
+      //   });
+      // }
 
 
-      function parallaxify() {
-        // margin-top shims to compensate for parallax as you progress further down the page 
-        var strategyShim = -20;
-        var designShim = 75;
-        var developmentShim = 150;
-        var summaryShim = 225;
+      // function parallaxify() {
+      //   // margin-top shims to compensate for parallax as you progress further down the page 
+      //   var strategyShim = -20;
+      //   var designShim = 75;
+      //   var developmentShim = 150;
+      //   var summaryShim = 225;
 
-        //Get scroll position of window
-        windowScroll = $(window).scrollTop();
-        var plaxFactor = 4;
-        //parallax the content section in front of the images
-        $strategy.css({
-          'margin-top' : (-windowScroll/plaxFactor + strategyShim)+'px'
-        });
-        $design.css({
-          'margin-top' : (-windowScroll/plaxFactor + designShim)+'px'
-        });
-        $development.css({
-          'margin-top' : (-windowScroll/plaxFactor + developmentShim)+'px'
-        });
-        $summary.css({
-          'margin-top' : (-windowScroll/plaxFactor + summaryShim)+'px'
-        });
-      }
+      //   //Get scroll position of window
+      //   windowScroll = $(window).scrollTop();
+      //   var plaxFactor = 4;
+      //   //parallax the content section in front of the images
+      //   $strategy.css({
+      //     'margin-top' : (-windowScroll/plaxFactor + strategyShim)+'px'
+      //   });
+      //   $design.css({
+      //     'margin-top' : (-windowScroll/plaxFactor + designShim)+'px'
+      //   });
+      //   $development.css({
+      //     'margin-top' : (-windowScroll/plaxFactor + developmentShim)+'px'
+      //   });
+      //   $summary.css({
+      //     'margin-top' : (-windowScroll/plaxFactor + summaryShim)+'px'
+      //   });
+      // }
 
     });
 
