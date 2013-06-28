d3aef5bf500411beeba6d66e0b1d3fc5c5479ccc
miknsh5 <mnash5@gmail.com>
2013-06-27 19:28:06 -0700

    icon transitions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,12 +3,30 @@
 angular.module('appliedByDesignApp')
     .controller('MainCtrl', function ($scope) {
       $scope.awesomeThings = ['hello', 'he', 'dsfg'];
-      
+
       // scroll to the strategy section
       $('#nav-scroll-down').click(function (){
         $.scrollTo('#nav-scroll-down',{duration: 'medium', offsetTop: '0'});
       });
+
+
+      // initialize icon visibilities
+      $scope.isStVisible = false;
+      $scope.isDsVisible = false;
+      $scope.isDvVisible = false;
+
+      $scope.toggleState = function(){
+        $scope.isStVisible = !$scope.isStVisible;
+        if ($scope.isStVisible) {
+          $('.block-lg').css({'width': '240px'});
+          $('.long-title').css({'opacity': 1});
+          $('.short-title').css({'opacity': 0});
+        } else {
+          $('.block-lg').css({'width': '80px'});
+          $('.long-title').css({'opacity': 0});
+          $('.short-title').css({'opacity': 1});
+        }
+
+      };
+
     });
-
-
-
