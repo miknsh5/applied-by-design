015ad1ccd34719c87fca92d2df02bfbacb4c658f
miknsh5 <mnash5@gmail.com>
2013-07-11 15:29:37 -0700

    added summary section

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,8 +4,10 @@
 angular.module('appliedByDesignApp')
     .controller('MainCtrl', function ($scope) {
 
-      // initialize Skrollr
-      skrollr.init();
+      // initialize Skrollr - only if not on a tablet or mobile device
+      if (screen.width > 1000){
+        skrollr.init();
+      }
 
       // smooth scroll to the strategy section
       $('#nav-scroll-down').click(function (){
@@ -47,74 +49,6 @@ angular.module('appliedByDesignApp')
         isMobile = true;
       }
 
-      // var $strategy = $('.section.strategy');
-      // var $design = $('.section.design');
-      // var $development = $('.section.development');
-      // var $summary = $('.section.summary');
-      // var windowScroll;
-
-      // // Check window width - if less than 1000px disable parallax
-      // function isLargeViewport() {
-      //   if($(window).width() < 979) {
-      //     return false;
-      //   } else {
-      //     return true;
-      //   }
-      // }
-
-      // // initialize the margin-tops to '0'.  Only adjust if browser passes
-      // $strategy.css({
-      //   'margin-top' : '0px'
-      // });
-      // $design.css({
-      //   'margin-top' : '0px'
-      // });
-      // $development.css({
-      //   'margin-top' : '0px'
-      // });
-      // $summary.css({
-      //   'margin-top' : '0px'
-      // });
-
-      // // recalc parallax offsets if window size changes
-      // $(window).resize(function() {
-      //     parallaxify();
-      //   });
-
-      // // If large viewport and not mobile, parallax the sections
-      // if(!isMobile) {
-      //   $(window).scroll(function() {
-      //     if(isLargeViewport()) {
-      //       parallaxify();
-      //     }
-      //   });
-      // }
-
-
-      // function parallaxify() {
-      //   // margin-top shims to compensate for parallax as you progress further down the page 
-      //   var strategyShim = -20;
-      //   var designShim = 75;
-      //   var developmentShim = 150;
-      //   var summaryShim = 225;
-
-      //   //Get scroll position of window
-      //   windowScroll = $(window).scrollTop();
-      //   var plaxFactor = 1;
-      //   //parallax the content section in front of the images
-      //   $strategy.css({
-      //     'margin-top' : (-windowScroll/plaxFactor + strategyShim)+'px'
-      //   });
-      //   $design.css({
-      //     'margin-top' : (-windowScroll/plaxFactor + designShim)+'px'
-      //   });
-      //   $development.css({
-      //     'margin-top' : (-windowScroll/plaxFactor + developmentShim)+'px'
-      //   });
-      //   $summary.css({
-      //     'margin-top' : (-windowScroll/plaxFactor + summaryShim)+'px'
-      //   });
-      // }
 
     });
 
