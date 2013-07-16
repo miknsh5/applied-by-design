bd7ec7e3118a0e02a50e0b3526e4725781e1c3e5
miknsh5 <mnash5@gmail.com>
2013-07-15 17:39:11 -0700

    added great circle routes from json

--- app/scripts/directives/world-map.js
+++ app/scripts/directives/world-map.js
@@ -14,6 +14,7 @@ angular.module('appliedByDesignApp')
 
         var width = scope.width;
         var height = scope.height;
+        var centered;
 
         // adapted from http://bl.ocks.org/mbostock/3757132
         var projection = d3.geo.mercator()
@@ -22,64 +23,81 @@ angular.module('appliedByDesignApp')
             .precision(.1);
 
         var path = d3.geo.path()
-            .projection(projection);
+            .projection(projection)
+            .pointRadius(function(d){ return [d.radius];});
+        // var airport = d3.geo.circle(),
+            // route = d3.geo.
 
+        var airportData = {'type': 'LineString', 'coordinates': [[-0.46, 51.5], [-73.8, 40.6]]}
+
+        // render the world map
         // element[0] references the containing directive DOM element
         var svg = d3.select(element[0]).append("svg")
             .attr("width", width)
             .attr("height", height);
 
+
+        var g = svg.append('g');
+
         d3.json("/images/world-50m.json", function(error, world) {
-          svg.insert("path")
+          g.insert("path")
               .datum(topojson.feature(world, world.objects.land))
               .attr("class", "land")
-              .attr("d", path);
+              .attr("d", path)
+              .on("click", clicked);
 
-          svg.insert("path")
+          g.insert("path")
               .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
               .attr("class", "boundary")
               .attr("d", path);
-        });
 
-        var cityPair = [
-            {
-                "longitude": -0.4619410038,
-                "latitude": 51.4706001282
-            },
-            {
-                "longitude": -73.7789,
-                "latitude": 40.6398
-            }
-        ];
-        // console.log(projection(testPt))
-        // "latitude_deg": "51.4706001282",
-        // "longitude_deg": "-0.4619410038",
+          d3.json('/images/AS-flightLegs.json', function(error, flightLegs){
 
-        // add city dot at specified coordinates
-        var city = svg.append('circle')
-            .attr('cx', 100)
-            .attr('cy', 200)
-            .attr('r', 50)
-            .attr('fill', 'blue')
-            .attr('stroke', 'red')
-            .attr('stroke-width', 2);
+            g.selectAll('route').data(flightLegs)
+                .enter()
+                    .append('path')
+                    .datum(function(d){
+                      return{'type': 'LineString', 'coordinates': [[d.origin.longitude_deg, d.origin.latitude_deg], [d.destination.longitude_deg, d.destination.latitude_deg]]};
+                    })
+                    .attr('class', 'route')
+                    .attr('fill', 'none')
+                    .attr('stroke', 'blue')
+                    .attr('stroke-width', 3)
+                    .attr('d', path);
+            });
 
 
+          })
+          
+        function clicked(d) {
+          var x, y, k;
 
-        var routeFunction = d3.svg.line()
-            .x(function(d) {return projection([d.longitude, d.latitude])[0];})
-            .y(function(d) {return projection([d.longitude, d.latitude])[1];})
-            .interpolate('linear');
+          if (d && centered !== d) {
+            var centroid = path.centroid(d);
+            x = centroid[0];
+            y = centroid[1];
+            k = 4;
+            centered = d;
+          } else {
+            x = width / 2;
+            y = height / 2;
+            k = 1;
+            centered = null;
+          }
 
-        var routes = svg.append('path')
-                        .attr('d', routeFunction(cityPair))
-                        .attr('stroke', 'blue')
-                        .attr('stroke-width', 2)
-                        .attr('fill', 'none');
+          g.selectAll("path")
+              .classed("route", centered && function(d) { return d === centered; });
 
-        d3.select(self.frameElement).style("height", height + "px");
+          g.transition()
+              .duration(750)
+              .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+              .style("stroke-width", 1.5 / k + "px");
+        }
+
+         
 
 
       }
+
     };
   });
