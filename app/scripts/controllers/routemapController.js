56a11caabc33b34f6a41f463a2d17018d7692608
miknsh5 <mnash5@gmail.com>
2013-07-20 12:36:49 -0700

    Dashboard section navigation

--- app/scripts/controllers/routemapController.js
+++ app/scripts/controllers/routemapController.js
@@ -5,11 +5,18 @@ angular.module('appliedByDesignApp')
     
     // load the data with route resolve function in app.js, don't load page until data is ready
     // this fixes the issue with async directive data binding
-    $scope.fleetRouteData = fleetRouteData;
 
     // set the dimensions of main SVG canvas
     $scope.mapWidth = 1200;
     $scope.mapHeight = 700;
+
+
+    // initialize dashboard panel visibility
+    $scope.isFinActive = true;
+    $scope.isOpsActive = false;
+    $scope.isFltActive = false;
+
+    $scope.fleetRouteData = fleetRouteData;
 
 
     // hardcode fleet models for now.  
@@ -20,6 +27,16 @@ angular.module('appliedByDesignApp')
         {'name': '737-800'},
         {'name': '737-900'}
     ];
+
+    $scope.financialSummary = [
+        {'name': 'Total Revenue', 'val': 4.65},
+        {'name': 'PAX Revenue', 'val': 3.29},
+        {'name': 'Operating Expense', 'val': 2.09},
+        {'name': 'RPM', 'val': 24.42},
+        {'name': 'ASM', 'val': 28.19},
+        {'name': 'RASM', 'val': 0.117},
+        {'name': 'CASM', 'val': 0.074}
+    ]
 
   });
 
