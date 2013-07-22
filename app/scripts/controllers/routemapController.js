ac60575db67c91aeb8e725a4ac4771469a574cc4
miknsh5 <mnash5@gmail.com>
2013-07-22 11:49:25 -0700

    toggle dashboard panel visibility

--- app/scripts/controllers/routemapController.js
+++ app/scripts/controllers/routemapController.js
@@ -5,12 +5,13 @@ angular.module('appliedByDesignApp')
 
     // set the dimensions of main SVG canvas
     $scope.mapWidth = 1200;
-    $scope.mapHeight = 700;
+    $scope.mapHeight = 1200;
 
     // initialize dashboard panel visibility
     $scope.isFinActive = true;
     $scope.isOpsActive = false;
     $scope.isFltActive = false;
+
 
     $scope.activeFinanceData = [];
 
@@ -18,6 +19,18 @@ angular.module('appliedByDesignApp')
     $scope.fleetModel = fleetModel;
     $scope.financialData = financialData;
 
+
+
+    // recalculate the dashboard panel position based on hide/show state
+    $scope.panelStyle = function(){
+        var offset = -250;
+        var b = ($scope.hidePanel) ? offset : 0;
+
+        return {
+            // width: $scope.mapWidth + 'px',
+            bottom: b + 'px'
+        }
+    }
   });
 
 
