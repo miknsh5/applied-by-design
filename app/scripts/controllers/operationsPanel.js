127a408e21a422f6f48c64ff7ea85c69fd5b850a
miknsh5 <mnash5@gmail.com>
2013-07-29 15:57:52 -0700

    Panel width updates

--- app/scripts/controllers/operationsPanel.js
+++ app/scripts/controllers/operationsPanel.js
@@ -3,11 +3,22 @@
 angular.module('appliedByDesignApp')
   .controller('OperationsPanelCtrl', function ($scope, navService, routeService, financialReports) {
     
+    // set the dimensions of chart SVG
+    $scope.chartWidth = 175;
+    $scope.chartHeight = 175;
 
-  	$scope.routeReport = routeService.setRouteReport('DENSEA');
+    $scope.routeChartData = [
+            {'name': 'A', 'val': 10},
+            {'name': 'B', 'val': 15},
+            {'name': 'C', 'val': 8},
+            {'name': 'D', 'val': 22}
+        ]
+
+
+    $scope.routeReport = routeService.setRouteReport('DENSEA');
 
     $scope.selectDay = function(id) {
-    	$scope.dotw = routeService.setActiveDay(id);
+        $scope.dotw = routeService.setActiveDay(id);
     }
     $scope.selectDay(0); //initialize selected day
 
