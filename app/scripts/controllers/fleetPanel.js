eec87c142796230fa810446e29a66eeddd2d8a8b
miknsh5 <mnash5@gmail.com>
2013-07-29 19:16:00 -0700

    fleetPanel bound to aircraft details

--- app/scripts/controllers/fleetPanel.js
+++ app/scripts/controllers/fleetPanel.js
@@ -1,12 +1,15 @@
 'use strict';
 
 angular.module('appliedByDesignApp')
-  .controller('FleetPanelCtrl', function ($scope, navService, fleetModel) {
+  .controller('FleetPanelCtrl', function ($scope, navService) {
   	
-	$scope.fleetModels = navService.getEquipment();
 	$scope.activeFleetModel = 0;
-	
+	$scope.fleet = navService.getEquipment();
+	$scope.fleetDetails = $scope.fleet[$scope.activeFleetModel].details;
+
 	$scope.selectModel = function(id){
 		$scope.activeFleetModel = navService.selectFleetModel(id);
+		$scope.fleetDetails = $scope.fleet[$scope.activeFleetModel].details;
+
 	}
   });
