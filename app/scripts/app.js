0ca27ef2b16451e00a3fbfff384d18b9a19392fa
miknsh5 <mnash5@gmail.com>
2013-08-28 11:57:27 -0700

    initial build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,11 @@
 'use strict';
 
-angular.module('appliedByDesignApp', [])
+angular.module('appliedByDesignApp', ['ngRoute'])
   .config(function ($routeProvider) {
+    $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/services', {
-        templateUrl: 'views/services.html',
-        controller: 'ServicesCtrl'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
-      })
-      .when('/blog', {
-        templateUrl: 'views/blog/index.html'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,49 +3,42 @@
 /*global skrollr:false */
 
 angular.module('appliedByDesignApp')
-    .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
 
-      // initialize Skrollr - only if not on a tablet or mobile device
-      if (screen.width > 1000){
-        skrollr.init({
-          forceHeight: false,
-          smoothScrolling: true
-        });
-      }
+    $scope.activePanel = 0;
 
-      // smooth scroll to the strategy section
-      $('#nav-arrow-1').click(function (){
-        $.scrollTo('#strategy-anchor',{duration: 'medium'});
-      });
+    $scope.showPanel = function(id){
+      $scope.activePanel = id;
 
-      $('#nav-arrow-2').click(function (){
-        $.scrollTo('#design-anchor',{duration: 'medium'});
-      });
+    }
 
-      $('#nav-arrow-3').click(function (){
-        $.scrollTo('#development-anchor',{duration: 'medium'});
-      });
+});
+      // // initialize Skrollr - only if not on a tablet or mobile device
+      // if (screen.width > 1000){
+      //   skrollr.init({
+      //     forceHeight: false,
+      //     smoothScrolling: true
+      //   });
+      // }
 
-      $('#nav-arrow-4').click(function (){
-        $.scrollTo('#contact-anchor',{duration: 'medium'});
-      });
+      // // smooth scroll to the strategy section
+      // $('#nav-arrow-1').click(function (){
+      //   $.scrollTo('#strategy-anchor',{duration: 'medium'});
+      // });
 
+      // $('#nav-arrow-2').click(function (){
+      //   $.scrollTo('#design-anchor',{duration: 'medium'});
+      // });
 
-      var isMobile;
+      // $('#nav-arrow-3').click(function (){
+      //   $.scrollTo('#development-anchor',{duration: 'medium'});
+      // });
 
-      //figure out which platform the user's on
-      if( navigator.userAgent.match(/Android/i) ||
-          navigator.userAgent.match(/webOS/i) ||
-          navigator.userAgent.match(/iPhone/i) ||
-          navigator.userAgent.match(/iPod/i) ||
-          navigator.userAgent.match(/iPad/i) ||
-          navigator.userAgent.match(/BlackBerry/)
-      ){
-        isMobile = true;
-      }
+      // $('#nav-arrow-4').click(function (){
+      //   $.scrollTo('#contact-anchor',{duration: 'medium'});
+      // });
 
 
-    });
 
 
 
