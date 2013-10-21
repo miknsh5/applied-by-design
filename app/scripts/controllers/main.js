7961d0bf406bda332250fce2601cc9ee4ef746e2
miknsh5 <mnash5@gmail.com>
2013-10-21 00:10:35 -0700

    IE9 css fix for greeting visibility

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,41 +19,14 @@ angular.module('appliedByDesignApp')
 
     $scope.fadeTitle = function(){
 
-        if (Modernizr.cssanimations) {
+        // if (Modernizr.cssanimations) {
           return {
             'margin-top': 300-($scope.scroll/3) + 'px',
             'opacity': 1-($scope.scroll/500)
           };
-        } else {
-            console.log('Whoops!!!')
-        }
+        // } 
     };
 
-    var timer;
-
-    function playCarousel(){
-      timer = setInterval(function(){
-        var next = $scope.activePanel + 1;
-
-        $scope.activePanel = next===3 ? 0 : next;
-        $scope.$apply();
-        // console.log($scope.activePanel);
-      }, 7000);
-    }
-
-    playCarousel();
-
-    $('#about-btn').click(function (){
-      $.scrollTo('#about-anchor',{duration: 'medium'});
-    });
-
-    $('#contact-btn').click(function (){
-      $.scrollTo('#contact-anchor',{duration: 'medium'});
-    });
-
-    $('#services-btn').click(function (){
-      $.scrollTo('#services-anchor',{duration: 'medium'});
-    });
 
   });
 
@@ -69,6 +42,7 @@ function isElementInViewport(elem) {
     // Get the position of the element on the page.
     var elemTop = Math.round( $elem.offset().top );
     var elemBottom = elemTop + $elem.height();
+    // console.log(viewportTop)
 
     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
 }
@@ -76,7 +50,6 @@ function isElementInViewport(elem) {
 // Check if it's time to start the animation.
 function checkAnimation(item) {
     var $elem = $(item);
-
     // If the animation has already been started
     // if ($elem.hasClass('anim-active')) return;
 
@@ -90,52 +63,68 @@ function checkAnimation(item) {
     // }
 }
 
+function checkGreetingVis(item) {
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+
+    var $elem = $(item);
+
+    if (viewportTop > 1000 ) {
+        // remove the class to hide the element
+        $elem.removeClass('anim-active');
+        $elem.css({'opacity': 0});
+        // console.log('removed it')
+    } else {
+        // add the anim class back so it's visibile at the top
+        $elem.addClass('anim-active');
+        $elem.css({'opacity': 1});
+        // console.log('added it')
+    }
+}
+
 function overrideAnim(item) {
     var $elem = $(item);
 
     $elem.addClass('anim-active');
 }
 
-if (!Modernizr.cssanimations){
-    // if browser doesn't support css animations then set them all to their 
-    // final state off the bat
-    overrideAnim('.dg1');
-    // overrideAnim('.con1');
-    // overrideAnim('.con2');
-    // overrideAnim('.con3');
-    // overrideAnim('.con4');
-    // overrideAnim('.con5');
-    // overrideAnim('.con6');
-    // overrideAnim('.fade-in-0');
-    // overrideAnim('.fade-in-1');
-    // overrideAnim('.fade-in-2');
-    // overrideAnim('.fade-in-3');
-    // overrideAnim('.fade-in-4');
-    // overrideAnim('.fade-in-5');
-    // overrideAnim('.fade-in-6');
-    overrideAnim('.bubble');
-    overrideAnim('.solution');
-}
+// if (!Modernizr.cssanimations){
+//     // if browser doesn't support css animations then set them all to their 
+//     // final state off the bat
+//     overrideAnim('.dg1');
+//     // overrideAnim('.con1');
+//     // overrideAnim('.con2');
+//     // overrideAnim('.con3');
+//     // overrideAnim('.con4');
+//     // overrideAnim('.con5');
+//     // overrideAnim('.con6');
+//     // overrideAnim('.fade-in-0');
+//     // overrideAnim('.fade-in-1');
+//     // overrideAnim('.fade-in-2');
+//     // overrideAnim('.fade-in-3');
+//     // overrideAnim('.fade-in-4');
+//     // overrideAnim('.fade-in-5');
+//     // overrideAnim('.fade-in-6');
+//     overrideAnim('.bubble');
+//     overrideAnim('.solution');
+// }
 
 // Capture scroll events
 $(window).scroll(function(){
 
     checkAnimation('.dg1');
-    // checkAnimation('.con1');
-    // checkAnimation('.con2');
-    // checkAnimation('.con3');
-    // checkAnimation('.con4');
-    // checkAnimation('.con5');
-    // checkAnimation('.con6');
     // checkAnimation('.fade-in-0');
-    // checkAnimation('.fade-in-1');
+    checkAnimation('.fade-in-1');
     checkAnimation('.fade-in-2');
     checkAnimation('.fade-in-3');
     checkAnimation('.fade-in-4');
     checkAnimation('.fade-in-5');
     checkAnimation('.fade-in-6');
+    checkAnimation('.fade-in-7');
     checkAnimation('.bubble');
     checkAnimation('.solution');
+    checkGreetingVis('.greeting');
 });
 
-checkAnimation('.fade-in-0');
+// checkAnimation('.fade-in-0');
+checkAnimation('.dg1');
