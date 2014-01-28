61f01ebeae8d688d51272cfef408a9c05ad82aec
miknsh5 <mnash5@gmail.com>
2014-01-28 13:06:21 -0800

    moving scroll functions into mainCtrl for IE9 issue

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -35,75 +35,81 @@ angular.module('appliedByDesignApp')
     $.scrollTo(0, 500);
   });
 
+  function isElementInViewport(elem) {
+    var $elem = $(elem);
 
-  resize();
-
-});
-
-
-function isElementInViewport(elem) {
-  var $elem = $(elem);
-
-  // Get the scroll position of the page.
-  var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') !== -1) ? 'body' : 'html');
-  var viewportTop = $(scrollElem).scrollTop();
-  var viewportBottom = viewportTop + $(window).height();
-
-  // Get the position of the element on the page.
-  var elemTop = Math.round($elem.offset().top);
-  var elemBottom = elemTop + $elem.height();
-  // console.log(viewportTop)
-
-  return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-}
-
-// Check if it's time to start the animation.
-function checkAnimation(item) {
-  var $elem = $(item);
-  // If the animation has already been started
-  // if ($elem.hasClass('anim-active')) return;
-  if (isElementInViewport($elem)) {
-    // Start the animation
-    $elem.addClass('anim-active');
-  }
-}
-
-function checkGreetingVis(item) {
-    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') !== -1) ? 'body' : 'html');
     var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
 
+    // Get the position of the element on the page.
+    var elemTop = Math.round($elem.offset().top);
+    var elemBottom = elemTop + $elem.height();
+    // console.log(viewportTop)
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+  }
+
+  // Check if it's time to start the animation.
+  function checkAnimation(item) {
     var $elem = $(item);
-
-    if (viewportTop > 1000 ) {
-        // remove the class to hide the element
-        $elem.removeClass('anim-active');
-        $elem.css({'opacity': 0});
-    } else {
-        // add the anim class back so it's visibile at the top
-        $elem.addClass('anim-active');
-        $elem.css({'opacity': 1});
+    // If the animation has already been started
+    // if ($elem.hasClass('anim-active')) return;
+    if (isElementInViewport($elem)) {
+      // Start the animation
+      $elem.addClass('anim-active');
     }
-}
+  }
 
-// Capture scroll events
-$(window).scroll(function () {
-  checkGreetingVis('.greeting');
-  checkAnimation('.fade-in-6');
-  checkAnimation('.fade-in-7');
+  function checkGreetingVis(item) {
+      var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+      var viewportTop = $(scrollElem).scrollTop();
+
+      var $elem = $(item);
+
+      if (viewportTop > 1000 ) {
+          // remove the class to hide the element
+          $elem.removeClass('anim-active');
+          $elem.css({'opacity': 0});
+      } else {
+          // add the anim class back so it's visibile at the top
+          $elem.addClass('anim-active');
+          $elem.css({'opacity': 1});
+      }
+  }
+
+  // Capture scroll events
+  $(window).scroll(function () {
+    checkGreetingVis('.greeting');
+    checkAnimation('.fade-in-6');
+    checkAnimation('.fade-in-7');
+  });
+
+
+  
+  function resize() {
+    var heights = window.innerHeight;
+    document.getElementById('landing').style.height = heights + 'px';
+    document.getElementById('first-section').style.marginTop = heights + 'px';
+    document.getElementById('scroll-btn').style.bottom = 60 + 'px';
+    var content = $('.greeting');
+    content.css({'margin-top': heights * 0.30 + 'px'});
+  }
+
+
+  window.onresize = function () {
+    resize();
+  };
+  resize();
+
+
+
 });
 
-function resize() {
-  var heights = window.innerHeight;
-  document.getElementById('landing').style.height = heights + 'px';
-  document.getElementById('first-section').style.marginTop = heights + 'px';
-  document.getElementById('scroll-btn').style.bottom = 60 + 'px';
-  var content = $('.greeting');
-  content.css({'margin-top': heights * 0.30 + 'px'});
-}
 
 
-window.onresize = function () {
-  resize();
-};
+
+
 
 
