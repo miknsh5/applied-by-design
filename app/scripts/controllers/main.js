ea9e9619d38ccc358385955007077f2aa515a4b9
miknsh5 <mnash5@gmail.com>
2014-01-30 16:17:43 -0800

    why IE9?

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,27 +12,27 @@ angular.module('appliedByDesignApp')
     $scope.fadeTitle = function () {
       console.log($scope.scroll);
       return {
-        'margin-top': (window.innerHeight /4 + $scope.scroll / 1.5) + 'px',
+        'margin-top': (window.innerHeight / 4 + $scope.scroll / 1.5) + 'px',
         'opacity': 1 - ($scope.scroll / 250)
       };
     };
   }
 
 
-  // $scope.$watch('scroll', function(oldPos, newPos){
-  //   // var $elem = $('#greeting');
-  //   // if ($elem.length === 1) {
-  //     if (newPos > 600 ){
-  //       $scope.showIntro = false;
-  //       // $elem.removeClass('anim-active');
-  //       // $elem.css({'opacity': 0 });
-  //     } else {
-  //       $scope.showIntro = true;
-  //       // $elem.addClass('anim-active');
-  //       // $elem.css({'opacity': 1 });
-  //     }
+  $scope.$watch('scroll', function(oldPos, newPos){
+    // var $elem = $('#greeting');
+    // if ($elem.length === 1) {
+      if (newPos > 600 ){
+        $scope.showIntro = false;
+        // $elem.removeClass('anim-active');
+        // $elem.css({'opacity': 0 });
+      } else {
+        $scope.showIntro = true;
+        // $elem.addClass('anim-active');
+        // $elem.css({'opacity': 1 });
+      }
     
-  // });
+  });
 
 });
 
