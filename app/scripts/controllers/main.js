e6f8d0544a3718cadff85add60fd0b85661ff8f4
miknsh5 <mnash5@gmail.com>
2014-02-10 16:33:45 -0800

    tech update, contacts reformat

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,6 @@ angular.module('appliedByDesignApp')
   
   if ( !Modernizr.touch ) { 
     $scope.fadeTitle = function () {
-      console.log($scope.scroll);
       return {
         'margin-top': (window.innerHeight / 4 + $scope.scroll / 1.5) + 'px',
         'opacity': 1 - ($scope.scroll / 250)
@@ -20,16 +19,10 @@ angular.module('appliedByDesignApp')
 
 
   $scope.$watch('scroll', function(oldPos, newPos){
-    // var $elem = $('#greeting');
-    // if ($elem.length === 1) {
       if (newPos > 600 ){
         $scope.showIntro = false;
-        // $elem.removeClass('anim-active');
-        // $elem.css({'opacity': 0 });
       } else {
         $scope.showIntro = true;
-        // $elem.addClass('anim-active');
-        // $elem.css({'opacity': 1 });
       }
     
   });
